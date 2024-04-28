7973294ca5cc0dc1b07198892ef624fd0156c999
Scott Ferguson <92749466+VizScott@users.noreply.github.com>
2024-04-28 13:54:16 -0500

    GitHub workflow added. Port logic in main. web.config build file in build.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/SFerguson90/FergusonAero/controllers"
 	"github.com/SFerguson90/FergusonAero/inits"
 	"github.com/gin-gonic/gin"
@@ -21,5 +23,11 @@ func main() {
 	r.PUT("/airplane/:id", controllers.UpdateAirplane)
 	r.DELETE("/airplane/:id", controllers.DeleteAirplane)
 
-	r.Run()
+	port := os.Getenv("HTTP_PLATFORM_PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Run("127.0.0.1:" + port)
 }
